c91269b88ee1cfa26a1fb823325516010caf1cc9
Jonathan <jreberg98@gmail.com>
2021-10-13 09:23:46 +0200

    Designa forsiden

--- Odsen/app/src/main/java/com/example/odsen/MainActivity.java
+++ Odsen/app/src/main/java/com/example/odsen/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.odsen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView loggedInn = findViewById(R.id.MAIN_logged_inn_user);
+        String userText = getString(R.string.MAIN_logged_inn_as, "dust");
+        loggedInn.setText(userText);
     }
 }
