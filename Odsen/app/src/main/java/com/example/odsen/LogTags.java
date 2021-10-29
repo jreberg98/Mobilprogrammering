9bb7d6745eeb3c7a436211b6eda127c7d1f089a5
Jonathan <jreberg98@gmail.com>
2021-10-29 10:59:38 +0200

    Logger inn på appen

---
+++ Odsen/app/src/main/java/com/example/odsen/LogTags.java
@@ -0,0 +1,6 @@
+package com.example.odsen;
+
+// Egen klasse for alle logtagger. Tanken er å gjøre det lettere å endre en tag dersom det skulle vært behov for det
+public class LogTags {
+    public static final String USER_LOGGING = "Innlogging";
+}
--- Odsen/app/src/main/java/com/example/odsen/MainActivity.java
+++ Odsen/app/src/main/java/com/example/odsen/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.odsen;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +32,17 @@ public class MainActivity extends AppCompatActivity {
     private Button MAIN_find_friends;
     private Button MAIN_create_room;
     private Button MAIN_finished_rooms;
+    private TextView loggedInn;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Navigering til de andre skjermene
         MAIN_open_rooms = findViewById(R.id.MAIN_open_rooms);
         MAIN_open_rooms.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,15 +90,94 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(finishedRoomsIntent);
             }
         });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        createAuthStateListener();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        TextView loggedInn = findViewById(R.id.MAIN_logged_inn_user);
+        loggedInn = findViewById(R.id.MAIN_logged_inn_user);
         String userText = getString(R.string.MAIN_logged_inn_as, "dust");
         loggedInn.setText(userText);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    private void createAuthStateListener() {
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    List<AuthUI.IdpConfig> providers = Arrays.asList(
+                            new AuthUI.IdpConfig.EmailBuilder().build()
+                    );
+
+                    signInLauncher.launch(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build()
+                    );
+                }
+            }
+        };
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            String username = user.getDisplayName();
+            String userText = "";
+
+            Log.i(LogTags.USER_LOGGING, "Logget inn som " + user.getEmail());
+
+            if (username != null) {
+                userText = getString(R.string.MAIN_logged_inn_as, username);
+            } else {
+                userText = getString(R.string.MAIN_logged_inn_as, "en navnløs bruker");
+            }
+
+            loggedInn.setText(userText);
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+
+            // Logger og avslutter appen
+            loggedInn.setText("VIRKER IKKE");
+            Log.e(LogTags.USER_LOGGING, "Innlogging feilet");
+            this.finishAffinity();
+
+        }
+    }
 
 }
